837d3f5f3fc1b5ae460fe0f47850c49e17755317
TranThienPhat123 <phattranthien915@gmail.com>
2025-03-19 15:11:29 +0700

    Add files via upload

---
+++ lab_03/caesar_cipher.py
@@ -0,0 +1,60 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+from ui.caesar import Ui_MainWindow
+import requests
+
+class MyApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.btn_Encrypt.clicked.connect(self.call_api_encrypt)
+        self.ui.btn_Decrypt.clicked.connect(self.call_api_decrypt)
+
+    def call_api_encrypt(self):
+        url = "http://127.0.0.1:5000/api/caesar/encrypt"
+        payload = {
+            "txt_Plaintext": self.ui.txt_Plaintext.toPlainText(),
+            "txt_Key": self.ui.txt_Key.toPlainText()
+        }
+        try:
+            response = requests.post(url, json=payload)
+            if response.status_code == 200:
+                data = response.json()
+                self.ui.textEdit_3.setPlainText(data["encrypted_message"])
+
+                msg = QMessageBox()
+                msg.setIcon(QMessageBox.Information)
+                msg.setText("Encrypted Successfully")
+                msg.exec_()
+            else:
+                print("Error while calling API")
+        except requests.exceptions.RequestException as e:
+            print("Error: %s" % e.message)
+
+    def call_api_decrypt(self):
+        url = "http://127.0.0.1:5000/api/caesar/decrypt"
+        payload = {
+            "txt_Ciphertext": self.ui.txt_Ciphertext.toPlainText(),
+            "txt_Key": self.ui.txt_Key.toPlainText()
+        }
+        try:
+            response = requests.post(url, json=payload)
+            if response.status_code == 200:
+                data = response.json()
+                self.ui.textEdit.setPlainText(data["decrypted_message"])
+
+                msg = QMessageBox()
+                msg.setIcon(QMessageBox.Information)
+                msg.setText("Decrypted Successfully")
+                msg.exec_()
+            else:
+                print("Error while calling API")
+        except requests.exceptions.RequestException as e:
+            print("Error: %s" % e.message)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+    sys.exit(app.exec_())
